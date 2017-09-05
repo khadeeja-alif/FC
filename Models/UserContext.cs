749838192017ce68b6124c003c9510f91c16d2ec
Farshan Ahamed <fffarshan8@outlook.com>
2017-09-05 12:30:40 +0530

    New API changes

---
+++ Models/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using FC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace FC.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MySQL.Data.EntityFrameworkCore.Extensions;
+using FC.Models;
+using System.Text.RegularExpressions;
+using System;
+
+namespace FC
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile("config.json", optional: true,reloadOnChange:true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            //services.AddDbContext<UserContext>(opt => opt.UseInMemoryDatabase());
+            services.AddMvc();
+#if DEBUG
+            var sqlConnectionString = Configuration.GetConnectionString("DataAccessMySqlProvider");
+#else
+              string envvariable = Environment.GetEnvironmentVariable("MYSQLCONNSTR_localdb");
+              string dbhost = "";
+              string dbname = "";
+              string dbusername = "";
+              string dbpassword = "";
+              dbhost = Regex.Replace(envvariable, "^.*Data Source=(.+?);.*$", "$1");
+              dbname = Regex.Replace(envvariable, "^.*Database=(.+?);.*$", "$1");
+              dbusername = Regex.Replace(envvariable, "^.*User Id=(.+?);.*$", "$1");
+              dbpassword = Regex.Replace(envvariable, "^.*Password=(.+?)$", "$1");
+              var host = dbhost.Split(':');
+              var sqlConnectionString = "server="+host[0]+";userid="+dbusername+";password="+dbpassword+";persistsecurityinfo=true;port="+host[1]+";database=markyourday;Sslmode=none";
+#endif
+            Console.WriteLine(sqlConnectionString);
+            System.Diagnostics.Debug.WriteLine(sqlConnectionString);
+            services.AddDbContext<UserContext>(options =>
+            {
+                options.UseMySQL(
+                    sqlConnectionString,
+                    b => b.MigrationsAssembly("FC")
+                );
+            });
+            services.AddDbContext<AttendanceContext>(options =>
+            {
+                options.UseMySQL(
+                    sqlConnectionString,
+                    b => b.MigrationsAssembly("FC")
+                );
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            //if(env.IsDevelopment())
+            //{
+                app.UseDeveloperExceptionPage();
+            //}
+            app.UseMvc();
+           /* app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+            */
+        }
+    }
+}
